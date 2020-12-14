259fa9d378b7da51188c8f6314aaecef56d3b52a
Roberto Polverelli Monti <rpolverelli@gmail.com>
2020-12-14 22:19:36 +0100

    add db migrations

--- app.py
+++ app.py
@@ -10,12 +10,51 @@
 """
 
 from flask import Flask, jsonify, abort, make_response, request
-from time import strftime, time
+from time import strftime, gmtime, time
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Job(db.Model):
+    job_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), index=True, unique=False)
+    complete = db.Column(db.Boolean)
+    elapsed = db.Column(db.Integer)
+    results = db.Column(db.Text)
+    submitted = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Job_id {}>'.format(self.job_id)
 
 
 jobs = []
+
+
+def get_human_time(seconds):
+    minutes = seconds // 60
+    hours = minutes // 60
+    time_str = ""
+    if hours > 0:
+        time_str += "{} hour".format(hours)
+        if hours > 1:
+            time_str += 's'
+        minutes = minutes % 60
+    if minutes > 0:
+        time_str += " {} minute".format(minutes)
+        if minutes > 1:
+            time_str += 's'
+        seconds = seconds % 60
+    if seconds >= 0 and hours < 1:
+        time_str += " {} second".format(seconds)
+        if seconds != 1:
+            time_str += 's'
+    return time_str.lstrip()
 
 
 @app.route('/api/v1.0/jobs/<int:job_id>', methods=['PUT'])
@@ -37,24 +76,7 @@ def post_results(job_id):
         'human': ""
     }
     job[0]['elapsed']['seconds'] = seconds
-    minutes = seconds // 60
-    hours = minutes // 60
-    job[0]['elapsed']['human'] = ""
-    if hours > 0:
-        job[0]['elapsed']['human'] += "{} hour".format(hours)
-        if hours > 1:
-            job[0]['elapsed']['human'] += 's'
-        minutes = minutes % 60
-    if minutes > 0:
-        job[0]['elapsed']['human'] += " {} minute".format(minutes)
-        if minutes > 1:
-            job[0]['elapsed']['human'] += 's'
-        seconds = seconds % 60
-    if seconds >= 0 and hours < 1:
-        job[0]['elapsed']['human'] += " {} second".format(seconds)
-        if seconds != 1:
-            job[0]['elapsed']['human'] += 's'
-    job[0]['elapsed']['human'] = job[0]['elapsed']['human'].lstrip()
+    job[0]['elapsed']['human'] = get_human_time(seconds)
 
     return jsonify({'job': job[0]})
 
@@ -65,30 +87,64 @@ def new_job():
     if not request.json or not 'title' in request.json:
         abort(400)
 
-    try:
-        job_id = jobs[-1]['id'] + 1
-    except IndexError:
-        job_id = 1
+    job_id = max([len(Job.query.all()), 1])
 
     job = {
         'id': job_id,
         'results': None,
-        'elapsed': None,
-        'title': request.json['title'],
+        'elapsed': {
+            'human': None,
+            'seconds': 0
+        },
+        'title': request.json['title'][:64],
         'complete': False,
         'submitted': {
             'human': strftime('%Y/%m/%d %H:%M'),
             'seconds': int(time())
-            }
+        }
     }
 
-    jobs.append(job)
+    # job_id = db.Column(db.Integer, primary_key=True)
+    # title = db.Column(db.String(64), index=True, unique=False)
+    # complete = db.Column(db.Boolean)
+    # elapsed = db.Column(db.Integer)
+    # results = db.Column(db.Text)
+    # submitted = db.Column(db.Integer)
+    db.session.add(Job(
+        job_id     =  job['id'],
+        title      =  job['title'],
+        complete   =  job['complete'],
+        elapsed    =  job['elapsed']['seconds'],
+        submitted  =  job['submitted']['seconds'],
+        results    =  job['results']
+    ))
+    db.session.commit()
 
     return jsonify({'job': job}), 201
 
 
 @app.route('/api/v1.0/jobs', methods=['GET'])
 def get_jobs():
+    jobs_query = Job.query.all()
+
+    jobs = []
+
+    for j in jobs_query:
+        k = {
+            'id': j.job_id,
+            'title': j.title,
+            'complete': False,
+            'results': j.results,
+            'elapsed': {
+                'seconds': j.elapsed,
+                'human': get_human_time(j.elapsed),
+            },
+            'submitted': {
+                'seconds': j.submitted,
+                'human': strftime('%Y/%m/%d %H:%M', gmtime(j.submitted)),
+            }
+        }
+
     return jsonify({'jobs': jobs})
 
 
